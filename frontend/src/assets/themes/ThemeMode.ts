c3416f1845a6c061e2931aa0e1ffd403b8613228
Codest-x <davinsontc@outlook.com>
2022-08-12 18:07:49 -0500

    PFS-1 feat: add ThemeMode file

---
+++ frontend/src/assets/themes/ThemeMode.ts
@@ -0,0 +1,19 @@
+const LightMode = {
+  name: 'light',
+  colors: {
+    primary: '#fff',
+    secondary: '#ff00aa',
+    tertiary: '#ffaa00',
+  },
+}
+
+const DarkMode = {
+  name: 'dark',
+  colors: {
+    primary: '#000',
+    secondary: '#ff00aa',
+    tertiary: '#ffaa00',
+  },
+}
+
+export { LightMode, DarkMode }
---
+++ frontend/src/assets/themes/index.ts
@@ -0,0 +1,3 @@
+import { LightMode, DarkMode } from './ThemeMode'
+
+export default { LightMode, DarkMode }
