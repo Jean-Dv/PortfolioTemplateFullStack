38adcfafd033d8b727507f6eeececa28d7f18f1e
Jean-Dv <mrjunior127@gmail.com>
2022-08-19 22:54:21 -0500

    PFS-5 Added new test for token restoring from database when token is not expired

--- __test__/auth/auth-success.spec.ts
+++ __test__/auth/auth-success.spec.ts
@@ -6,6 +6,8 @@ import { MongoService } from '../../services/mongoDb'
 
 jest.mock('../../api/auth/service')
 
+let token = ''
+
 beforeEach(async () => {
   const authController = AuthController.instance
   const newUser = {
@@ -43,8 +45,22 @@ describe('AUTH CRUD WITH JWT TOKEN (SUCCESS)', () => {
       .post(`${routePrefix}/auth/register`)
       .set('Accept', 'application/json')
       .send(newUser)
+    const resLogin = await request(appServer)
+      .post(`${routePrefix}/auth/login`)
+      .set('Accept', 'application')
+      .send({ email: newUser.email, password: newUser.password })
     expect(responseSignUp.statusCode).toBe(201)
     expect(responseSignUp.body.data).toMatchObject(expectedSignUp)
+    expect(resLogin.statusCode).toBe(200)
+    expect(resLogin.body.data.token).toMatch(/^([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_\-+/=]*)/)
+    token = resLogin.body.data.token
+  })
+  test('should return 200 and token restoring from database', async () => {
+    const responseSignIn = await request(appServer)
+      .post(`${routePrefix}/auth/login`)
+      .send({ email: 'mrjunior127@gmail.com', password: '12345' })
+    expect(responseSignIn.body.data.token).toMatch(/^([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_=]+)\.([a-zA-Z0-9_\-+/=]*)/)
+    expect(responseSignIn.body.data.token).toContain(token)
   })
 })
 
