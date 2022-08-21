8af09e05774a78aaaafc2b27fc4ec67a0992ed2d
Jean-Dv <mrjunior127@gmail.com>
2022-08-21 18:59:08 -0500

    PFS-12 added new interfaces and types for repository controller

--- api/github-repos/types.d.ts
+++ api/github-repos/types.d.ts
@@ -6,6 +6,22 @@ export interface IRepository extends Document {
   url: string
   name: string
   description: string
+  language: string
   stars: number
   forks: number
 }
+
+export interface RepositoryInterface {
+  _id: Types.ObjectId
+  github_id: number
+  url: string
+  name: string
+  description: string
+  language: string
+  stars: number
+  forks: number
+  createdAt: string
+  updatedAt: string
+}
+
+export type NewRepository = Omit<RepositoryInterface, '_id' | 'createdAt' | 'updatedAt'>
