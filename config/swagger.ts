fb201e98bb269a08d3934a12936791c7787c17ee
Jean-Dv <mrjunior127@gmail.com>
2022-08-25 12:20:25 -0500

    Added config for swagger

---
+++ config/swagger.ts
@@ -0,0 +1,32 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+import { Logger } from 'log4js'
+import { Application, Response, Request } from 'express'
+
+const options = {
+  definitions: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Portfolio Fullstack API',
+      version: '1.0.0'
+    },
+    apis: [
+      './api/github-repos/router.ts', './api/github-repos/model.ts'
+    ]
+  }
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+export const swaggerDocs = (app: Application, port: string | number, routePrefix: string, logger: Logger): void => {
+  let protocol: string = 'http'
+  let hostname: string = 'localhost'
+  app.use(`${routePrefix}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+  app.get(`${routePrefix}/docs.json`, (req: Request, res: Response) => {
+    protocol = req.protocol
+    hostname = req.headers.host as string
+    res.setHeader('Content-Type', 'application/json')
+    res.send(swaggerSpec)
+  })
+  logger.info(`Version 1 Docs are available on ${protocol}://${hostname}:${port}${routePrefix}/docs`)
+}
